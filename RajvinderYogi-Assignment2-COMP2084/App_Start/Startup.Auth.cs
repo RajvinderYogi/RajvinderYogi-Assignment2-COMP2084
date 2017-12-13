b07a9f83bde229bdce003fb509e6675220a0d29b
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-12-12 22:55:06 -0500

    added google login

--- RajvinderYogi-Assignment2-COMP2084/App_Start/Startup.Auth.cs
+++ RajvinderYogi-Assignment2-COMP2084/App_Start/Startup.Auth.cs
@@ -6,6 +6,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Owin;
 using RajvinderYogi_Assignment2_COMP2084.Models;
+using System.Configuration;
 
 namespace RajvinderYogi_Assignment2_COMP2084
 {
@@ -58,11 +59,11 @@ namespace RajvinderYogi_Assignment2_COMP2084
             //   appId: "",
             //   appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = ConfigurationManager.AppSettings["GoogleClientId"],
+                ClientSecret = ConfigurationManager.AppSettings["GoogleClientSecret"]
+            });
         }
     }
 }
