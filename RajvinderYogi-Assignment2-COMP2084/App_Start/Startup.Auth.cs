db4171218a415e61ee79ae6c0770736b78491f28
RajvinderYogi <rajvinderyogi@yahoo.com>
2017-12-13 11:45:29 -0500

    Fb & Microsoft login done

--- RajvinderYogi-Assignment2-COMP2084/App_Start/Startup.Auth.cs
+++ RajvinderYogi-Assignment2-COMP2084/App_Start/Startup.Auth.cs
@@ -47,17 +47,17 @@ namespace RajvinderYogi_Assignment2_COMP2084
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Uncomment the following lines to enable logging in with third party login providers
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            app.UseMicrosoftAccountAuthentication(
+                clientId: ConfigurationManager.AppSettings["MicrosoftClientId"],
+                clientSecret: ConfigurationManager.AppSettings["MicrosoftClientSecret"]);
 
             //app.UseTwitterAuthentication(
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             {
